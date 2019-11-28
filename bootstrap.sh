7fc40b102168c2d8676ebfd8a78e50839ed7d046
Benoit Suttor <bsuttor@imio.be>
2019-11-28 10:42:25 +0100

    Remove unused file

--- bootstrap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Usage:
-#     ./bootstrap.sh  # use buildout.cfg
-#     ./bootstrap.sh -c dev.cfg  # use dev.cfg
-ln -s dev.cfg buildout.cfg
-if [ -f /usr/bin/virtualenv-2.7 ] ; then virtualenv-2.7 .;else virtualenv -p python2.7 .;fi
-bin/pip install -I -r requirements.txt
-bin/buildout "$@"
